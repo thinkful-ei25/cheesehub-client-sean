a46c409b7995186904b952f1e049c5cc894a4743
Sean Nealon <signalflowsean@gmail.com>
2018-11-16 11:36:28 -0500

    cheese-list component displays cheeses

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CheeseList from './components/cheese-list'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cheeses =  [
+  "Bath Blue",
+  "Barkham Blue",
+  "Buxton Blue",
+  "Cheshire Blue",
+  "Devon Blue",
+  "Dorset Blue Vinney",
+  "Dovedale",
+  "Exmoor Blue",
+  "Harbourne Blue",
+  "Lanark Blue",
+  "Lymeswold",
+  "Oxford Blue",
+  "Shropshire Blue",
+  "Stichelton",
+  "Stilton",
+  "Blue Wensleydale",
+  "Yorkshire Blue"
+]
+
+ReactDOM.render(<CheeseList cheeses={cheeses} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
